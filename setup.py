b399226d3d5d20b4cc68ebaef0fd4e8809b21761
st1vms <96783398+st1vms@users.noreply.github.com>
2023-08-23 11:13:56 +0200

    Absoulte paths

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
+import os
 
-with open("requirements.txt", "r") as fp:
+with open(os.path.join(os.getcwd(), "requirements.txt"), "r") as fp:
     install_requires = fp.read().splitlines()
 
 __DESCRIPTION = """Unofficial Claude2 API supporting direct HTTP chat creation/deletion/retrieval, \
 message attachments and auto session gathering using Firefox with geckodriver. \
 """
 
-with open("README.md", "r") as fp:
+with open(os.path.join(os.getcwd(), "README.md"), "r") as fp:
     __LONG_DESCRIPTION = fp.read().lstrip().rstrip()
 
 setup(
