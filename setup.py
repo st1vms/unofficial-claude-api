88b7f2f52608dfd569be2cee2ead37dada6293d2
st1vms <96783398+st1vms@users.noreply.github.com>
2023-08-23 10:37:08 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().splitlines()
+with open("requirements.txt", "r") as fp:
+    install_requires = fp.read().splitlines()
 
 __DESCRIPTION = """Unofficial Claude2 API supporting direct HTTP chat creation/deletion/retrieval, \
 message attachments and auto session gathering using Firefox with geckodriver. \
 """
 
+with open("README.md", "r") as fp:
+    __LONG_DESCRIPTION = fp.read().lstrip().rstrip()
+
 setup(
     name="unofficial-claude2-api",
     version="0.1.0",
     author="st1vms",
+    author_email="stefano.maria.salvatore@gmail.com",
     description=__DESCRIPTION,
+    long_description=__LONG_DESCRIPTION,
+    url="https://github.com/st1vms/unofficial-claude2-api",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
     install_requires=install_requires,
 )
