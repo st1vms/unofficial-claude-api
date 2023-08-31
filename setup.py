52a617ba8bca2fb708b18b958204b3a74c597a9f
st1vms <96783398+st1vms@users.noreply.github.com>
2023-08-31 09:51:01 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(join(abspath(dirname(__file__)), "README.md"), "r") as fp:
 
 setup(
     name="unofficial-claude2-api",
-    version="0.1.6",
+    version="0.1.7",
     author="st1vms",
     author_email="stefano.maria.salvatore@gmail.com",
     description=__DESCRIPTION,
