7de5bb07370fb04bc8aa416c04f730cce8b71393
st1vms <96783398+st1vms@users.noreply.github.com>
2023-12-05 14:40:55 +0100

    setup.py | version 0.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(
 
 setup(
     name="unofficial-claude2-api",
-    version="0.2.0",
+    version="0.2.1",
     author="st1vms",
     author_email="stefano.maria.salvatore@gmail.com",
     description=__DESCRIPTION,
