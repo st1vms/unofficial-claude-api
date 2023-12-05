5d007f47492bd25d07d45cc1168847fad9f1c24f
st1vms <96783398+st1vms@users.noreply.github.com>
2023-12-05 14:46:44 +0100

    v0.2.2 | Reverted workflow changes, removed requirements.txt

--- setup.py
+++ setup.py
@@ -14,18 +14,9 @@ with open(
 ) as fp:
     __LONG_DESCRIPTION = fp.read().lstrip().rstrip()
 
-with open(
-    join(abspath(dirname(__file__)), "requirements.txt"),
-    "r",
-    encoding="utf-8",
-    errors="ignore",
-) as fp:
-    __REQUIREMENTS = [line.strip() for line in fp.readlines() if line]
-
-
 setup(
     name="unofficial-claude2-api",
-    version="0.2.1",
+    version="0.2.2",
     author="st1vms",
     author_email="stefano.maria.salvatore@gmail.com",
     description=__DESCRIPTION,
@@ -39,5 +30,11 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    install_requires=__REQUIREMENTS,
+    install_requires=[
+        "curl_cffi",
+        "selenium",
+        "screeninfo",
+        "tzlocal",
+        "brotli",
+    ],
 )
